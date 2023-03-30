8e113bd09e40af7ea49875516c012f760353f99f
한관수 <jin9776@gmail.com>
2023-03-30 17:54:47 +0900

    add - snmp

---
+++ sol-module-snmp/src/main/java/sol/link/solmodulesnmp/SolModuleSnmpApplication.java
@@ -0,0 +1,13 @@
+package sol.link.solmodulesnmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolModuleSnmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolModuleSnmpApplication.class, args);
+    }
+
+}
---
+++ sol-module-snmp/src/test/java/sol/link/solmodulesnmp/SolModuleSnmpApplicationTests.java
@@ -0,0 +1,13 @@
+package sol.link.solmodulesnmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolModuleSnmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
