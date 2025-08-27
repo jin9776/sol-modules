7e49c94d0977af30ca38e9c8babc8b77dda5d8ba
한관수 <48613026+jin9776@users.noreply.github.com>
2025-08-27 17:49:59 +0900

    mod - 표시 순서 변경

--- sol-module-perfmon/src/main/java/sol/link/perfmon/dto/PerfDto.java
+++ sol-module-perfmon/src/main/java/sol/link/perfmon/dto/PerfDto.java
@@ -12,32 +12,8 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class PerfDto {
 
-    private int coreCnt;
-    private double cpuLoad;
-
-    private long memTotal;
-    private long memUse;
-    @Setter(AccessLevel.PRIVATE)
-    private double memUseRate;
-    public double getMemUseRate() {
-        return Math.round(100d * memUse / memTotal * 100) / 100.0;
-    }
-
-    private long heapTotal;
-    private long heapUse;
-    @Setter(AccessLevel.PRIVATE)
-    private double heapUseRate;
-    public double getHeapUseRate() {
-        return Math.round(100d * heapUse / heapTotal * 100) / 100.0;
-    }
-
-    private int processThreadCnt;
-    private long processPid;
-    private String processName;
     @Setter(AccessLevel.PRIVATE)
     private double processCpuLoad;
-    private double processCoreCpuLoad;
-    private double processMemUseRate;
     public double getProcessCpuLoad() {
         if (coreCnt > 0) {
             return Math.round(processCoreCpuLoad / coreCnt * 100) / 100.0;
@@ -45,7 +21,35 @@ public class PerfDto {
         return processCoreCpuLoad;
     }
 
+    @Setter(AccessLevel.PRIVATE)
+    private double heapUseRate;
+    public double getHeapUseRate() {
+        return Math.round(100d * heapUse / heapTotal * 100) / 100.0;
+    }
+
+    private double processMemUseRate;
     public double getProcessMemUseRate() {
         return Math.round(100d * processMemUseRate / memTotal * 100) / 100.0;
     }
+
+    private double processCoreCpuLoad;
+    private int coreCnt;
+
+    private double cpuLoad;
+    @Setter(AccessLevel.PRIVATE)
+    private double memUseRate;
+    public double getMemUseRate() {
+        return Math.round(100d * memUse / memTotal * 100) / 100.0;
+    }
+
+    private long memTotal;
+    private long memUse;
+
+    private long heapTotal;
+    private long heapUse;
+
+
+    private int processThreadCnt;
+    private long processPid;
+    private String processName;
 }
